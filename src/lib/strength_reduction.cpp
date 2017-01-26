a164d42f1d86eacd5f8ec09bc70c483bf6e10df6
Patrick Kreutzer <patrick.kreutzer@fau.de>
2017-01-26 18:08:21 +0100

    Implement very simple strength reduction pass.

---
+++ src/lib/strength_reduction.cpp
@@ -0,0 +1,57 @@
+#include "lib/strength_reduction.h"
+
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+
+char StrengthReduction::ID = 0;
+
+static int isConst2(const BinaryOperator *binaryOperator, const int operand) {
+  ConstantInt *constant;
+  return ((constant = dyn_cast<ConstantInt>(binaryOperator->getOperand(operand))) &&
+          (constant->getZExtValue() == 2));
+}
+
+static int operandToShift(const BinaryOperator *binaryOperator) {
+  if (isConst2(binaryOperator, 0)) return 1;
+  if (isConst2(binaryOperator, 1)) return 0;
+  return -1;
+}
+
+bool StrengthReduction::runOnFunction(Function &function) {
+  bool modified = false;
+
+  for (auto itBasicBlock = function.begin(); itBasicBlock != function.end(); ++itBasicBlock) {
+    for (auto itInstruction = itBasicBlock->begin(); itInstruction != itBasicBlock->end();) {
+      Instruction *instruction = &*itInstruction++;
+      BinaryOperator *binaryOperator;
+
+      if ((binaryOperator = dyn_cast<BinaryOperator>(instruction)) &&
+          (binaryOperator->getOpcode() == Instruction::Mul)) {
+
+        int _operandToShift = operandToShift(binaryOperator);
+        if (_operandToShift != -1) {
+          BinaryOperator *shl = BinaryOperator::CreateShl(
+            binaryOperator->getOperand(_operandToShift),
+            ConstantInt::getSigned(binaryOperator->getType(), 1),
+            "", binaryOperator);
+          shl->takeName(binaryOperator);
+          binaryOperator->replaceAllUsesWith(shl);
+          binaryOperator->eraseFromParent();
+
+          modified = true;
+        }
+      }
+    }
+  }
+
+  return modified;
+}
+
+static bool MODIFIES_CFG = false;
+static bool ANALYSIS_ONLY = false;
+
+static RegisterPass<StrengthReduction>
+  strengthReduction("strength-reduction", "performs a very simple strength reduction",
+                    MODIFIES_CFG, ANALYSIS_ONLY);
---
+++ src/lib/strength_reduction.h
@@ -0,0 +1,24 @@
+#ifndef __INCLUDE_STRENGTH_REDUCTION
+#define __INCLUDE_STRENGTH_REDUCTION
+
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/raw_ostream.h"
+
+#include <string>
+#include <map>
+
+using namespace llvm;
+
+struct StrengthReduction : public FunctionPass {
+
+  static char ID;
+
+  StrengthReduction() : FunctionPass(ID) {}
+
+  bool runOnFunction(Function &function) override;
+
+};
+
+#endif
