8cb5fd324a8f90e976d7e8b29fa12c26e7ea81bc
Patrick Kreutzer <patrick.kreutzer@fau.de>
2017-01-27 12:43:58 +0100

    Make the strength reduction pass inherit from BasicBlockPass.

--- src/lib/strength_reduction.h
+++ src/lib/strength_reduction.h
@@ -2,7 +2,7 @@
 #define __INCLUDE_STRENGTH_REDUCTION
 
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
 
@@ -11,13 +11,13 @@
 
 using namespace llvm;
 
-struct StrengthReduction : public FunctionPass {
+struct StrengthReduction : public BasicBlockPass {
 
   static char ID;
 
-  StrengthReduction() : FunctionPass(ID) {}
+  StrengthReduction() : BasicBlockPass(ID) {}
 
-  bool runOnFunction(Function &function) override;
+  bool runOnBasicBlock(BasicBlock &basicBlock) override;
 
 };
 
